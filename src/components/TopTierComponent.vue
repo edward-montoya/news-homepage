822dcf9f4199639940172ecb7ceaebfaa1359917
Edward Montoya <edward.nmontoya@gmail.com>
2023-07-10 08:31:02 -0500

    Added missing alt images

--- src/components/TopTierComponent.vue
+++ src/components/TopTierComponent.vue
@@ -1,13 +1,13 @@
 <template>
   <section class="top">
     <article class="top__container">
-      <img class="top__image" src="../assets/images/image-retro-pcs.jpg" />
+      <img class="top__image" src="../assets/images/image-retro-pcs.jpg" alt="Retro PCs image" />
       <span class="top__numeration">01</span>
       <h3 class="top__title"><a href="/posts/reviving-retro-pcs/346453">Reviving Retro PCs</a></h3>
       <p class="top__description">What happens when old PCs are given modern upgrades?</p>
     </article>
     <article class="top__container">
-      <img class="top__image" src="../assets/images/image-top-laptops.jpg" />
+      <img class="top__image" src="../assets/images/image-top-laptops.jpg" alt="Futuristic laptop" />
       <span class="top__numeration">02</span>
       <h3 class="top__title">
         <a href="/posts/top-10-laptos-2022/234232">Top 10 Laptops of 2022</a>
@@ -15,7 +15,7 @@
       <p class="top__description">Our best picks for various needs and budgets.</p>
     </article>
     <article class="top__container">
-      <img class="top__image" src="../assets/images/image-gaming-growth.jpg" />
+      <img class="top__image" src="../assets/images/image-gaming-growth.jpg" alt="Gamming PC" />
       <span class="top__numeration">03</span>
       <h3 class="top__title"><a href="/posts/grow-gaming/32424">The Grownth of Gaming</a></h3>
       <p class="top__description">How the pandemic has sparked fresh opportunities.</p>
